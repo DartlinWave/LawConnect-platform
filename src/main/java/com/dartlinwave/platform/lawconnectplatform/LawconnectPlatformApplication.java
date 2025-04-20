99f94123247e137854b4973ac8866d2a245064d9
CamilaLoli <u202110385@upc.edu.pe>
2025-04-20 00:44:44 -0500

    Initial commit

---
+++ src/main/java/com/dartlinwave/platform/lawconnectplatform/LawconnectPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dartlinwave.platform.lawconnectplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawconnectPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawconnectPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dartlinwave/platform/lawconnectplatform/LawconnectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dartlinwave.platform.lawconnectplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawconnectPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
