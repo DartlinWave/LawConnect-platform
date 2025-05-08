7011e8e0640509014ff0b5c74b4697b04fea2e32
CamilaLoli <u202110385@upc.edu.pe>
2025-05-08 15:28:16 -0500

    feat(matchmaking): update command service with new additions.

--- src/main/java/com/dartlinwave/platform/lawconnectplatform/matchmaking/domain/services/MatchCommandService.java
+++ src/main/java/com/dartlinwave/platform/lawconnectplatform/matchmaking/domain/services/MatchCommandService.java
@@ -1,12 +1,12 @@
 package com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.services;
 
-import com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.model.commands.CreatePreMatchCommand;
-import com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.model.commands.DeclineCaseByLawyerCommand;
-import com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.model.commands.DeclineLawyerByClientCommand;
+import com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.model.commands.*;
 
 public interface MatchCommandService {
     Long handle (CreatePreMatchCommand command);
 
+    void handle (AcceptCaseByLawyerCommand command);
+    void handle (AcceptLawyerByClientCommand command);
     void handle (DeclineCaseByLawyerCommand command);
     void handle (DeclineLawyerByClientCommand  command);
 }
