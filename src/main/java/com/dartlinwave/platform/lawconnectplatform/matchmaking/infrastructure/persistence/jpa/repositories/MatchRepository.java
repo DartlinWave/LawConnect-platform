b996caf1de14658dd2cd866b1bbdb7b91a31ab13
CamilaLoli <u202110385@upc.edu.pe>
2025-05-09 07:22:23 -0500

    feat(matchmaking): update repository.

--- src/main/java/com/dartlinwave/platform/lawconnectplatform/matchmaking/infrastructure/persistence/jpa/repositories/MatchRepository.java
+++ src/main/java/com/dartlinwave/platform/lawconnectplatform/matchmaking/infrastructure/persistence/jpa/repositories/MatchRepository.java
@@ -1,14 +1,19 @@
 package com.dartlinwave.platform.lawconnectplatform.matchmaking.infrastructure.persistence.jpa.repositories;
 
 import com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.model.aggregates.Match;
+import com.dartlinwave.platform.lawconnectplatform.matchmaking.domain.model.valueobjects.MatchStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
-    Optional<Match> findMatchByCaseIdAndLawyerId(Long caseId, Long lawyerId);
+    Optional<Match> findByCaseIdAndLawyerId(Long caseId, Long lawyerId);
+    Optional<Match> findByLawyerId(Long lawyerId);
+    Optional<Match> findByCaseId(Long caseId);
 
+    List<Match> findAllByMatchStatusAndLawyerId(MatchStatus matchStatus, Long lawyerId);
+    List<Match> findAllByMatchStatusAndCaseId(MatchStatus matchStatus, Long caseId);
     List<Match> findAllByLawyerId(Long lawyerId);
     List<Match> findAllByCaseId(Long caseId);
 }
