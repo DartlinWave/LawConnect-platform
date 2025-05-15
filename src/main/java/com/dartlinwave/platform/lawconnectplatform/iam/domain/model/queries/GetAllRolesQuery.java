264074fe4d0136dd94141a7c6706b373b6c7323e
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-05-14 23:03:34 -0500

    feat(iam): add get all roles query.

---
+++ src/main/java/com/dartlinwave/platform/lawconnectplatform/iam/domain/model/queries/GetAllRolesQuery.java
@@ -0,0 +1,10 @@
+package com.dartlinwave.platform.lawconnectplatform.iam.domain.model.queries;
+
+/**
+ * Query object for requesting all roles in the system.
+ * <p>
+ * Used by the service layer to retrieve the complete set of roles.
+ * </p>
+ */
+public record GetAllRolesQuery() {
+}
